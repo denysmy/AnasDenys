6a8908f9b51a57a6c2a37ee5e8ce2cd60478b0b0
Anas <morocco1991@gmail.com>
2020-05-08 16:49:27 -0400

    new

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, world");
-        System.out.println("Hello baby");
+        System.out.println("Hello guy");
     }
 
 }
